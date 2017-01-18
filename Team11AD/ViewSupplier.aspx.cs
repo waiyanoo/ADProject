63cd6075ab2735aadc11fb1793534c479033e568
Wai Yan Oo <e0046649@u.nus.edu>
2017-01-18 22:35:42 +0800

    gridview

--- Team11AD/ViewSupplier.aspx.cs
+++ Team11AD/ViewSupplier.aspx.cs
@@ -4,12 +4,37 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DataAccess;
 
 namespace Team11AD
 {
     public partial class ViewSupplier : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        {
+            gvsupplier.DataSource = GetItem();
+            gvsupplier.DataBind();
+        }
+
+        public List<Supplier> GetItem()
+        {
+            LogicUniversityEntities context = new LogicUniversityEntities();
+            return context.Suppliers.ToList<Supplier>();
+        }
+
+        protected void gvsupplier_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void gvsupplier_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            GridViewRow row = (GridViewRow)gvsupplier.Rows[e.RowIndex];
+            
+           
+        }
+
+        protected void gvsupplier_RowEditing(object sender, GridViewEditEventArgs e)
         {
 
         }
