a419b01ae3fe937bb8b5077d389288ca9a91b3be
Wai Yan Oo <e0046649@u.nus.edu>
2017-01-22 22:13:24 +0800

    approve adjustment

--- BusinessLogic/ApproveAdjustmentBL.cs
+++ BusinessLogic/ApproveAdjustmentBL.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DataAccess;
+using BusinessObject;
 
 namespace BusinessLogic
 {
@@ -13,6 +14,24 @@ namespace BusinessLogic
         {
             ApproveAdjustmentDA rada = new ApproveAdjustmentDA();
             return rada.getAdjustmentNo();
+        }
+
+        public List<ApproveAdjustmentBO> GetItemabove(string id)
+        {
+            ApproveAdjustmentDA rada = new ApproveAdjustmentDA();
+            return rada.getAdjustmentNoAbove(id);
+        }
+
+        public List<ApproveAdjustmentBO> GetItembelow(string id)
+        {
+            ApproveAdjustmentDA rada = new ApproveAdjustmentDA();
+            return rada.getAdjustmentNoBelow(id);
+        }
+
+        public void ApproveItem(string voucher, string item, string status)
+        {
+            ApproveAdjustmentDA rada = new ApproveAdjustmentDA();
+            rada.ApproveItem(voucher, item, status);
         }
     }
 }
---
+++ BusinessObject/ApproveAdjustmentBO.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class ApproveAdjustmentBO
+    {
+        private string itemID;
+        private string cat;
+        private string description;
+        private string qty;
+        private string price;
+
+       
+        public ApproveAdjustmentBO()
+        {
+
+        }
+
+        public ApproveAdjustmentBO(string itemID, string cat, string description, string qty, string price)
+        {
+            this.itemID = itemID;
+            this.cat = cat;
+            this.description = description;
+            this.qty = qty;
+            this.price = price;
+        }
+
+        public string ItemID
+        {
+            get
+            {
+                return itemID;
+            }
+
+            set
+            {
+                itemID = value;
+            }
+        }
+
+        
+
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+
+            set
+            {
+                description = value;
+            }
+        }
+
+        public string Qty
+        {
+            get
+            {
+                return qty;
+            }
+
+            set
+            {
+                qty = value;
+            }
+        }
+
+        public string Price
+        {
+            get
+            {
+                return price;
+            }
+
+            set
+            {
+                price = value;
+            }
+        }
+
+        public string Cat
+        {
+            get
+            {
+                return cat;
+            }
+
+            set
+            {
+                cat = value;
+            }
+        }
+    }
+}
