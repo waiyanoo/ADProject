18906e39192896ec0fc66a59862b1e69cc6ee337
EiYadanarKyaw <eiyadanar94@gmail.com>
2017-01-22 21:55:07 +0800

    Ei All BO updates

---
+++ BusinessObject/CollectionPointBO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class CollectionPointBO
+    {
+        private string collectionPointName;
+        private UserBO userID;
+    }
+}
