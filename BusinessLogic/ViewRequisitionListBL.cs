5c28cbc5f03d7815e398fff7166821d90238c244
Wai Yan Oo <e0046649@u.nus.edu>
2017-01-23 15:34:49 +0800

    1-23 update

---
+++ BusinessLogic/ViewRequisitionListBL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using BusinessObject;
+
+namespace BusinessLogic
+{
+    public class ViewRequisitionListBL
+    {
+        public List<ViewRequsitionListBO> FindAllRequisition()
+        {
+            ViewRequisitionListDA vrda = new ViewRequisitionListDA();
+            return vrda.FindAllRequisition();
+        }
+    }
+}
