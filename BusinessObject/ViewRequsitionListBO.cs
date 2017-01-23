e9a91edb88a2d4e350cfa4f2d306fb979dd2bb75
LIN SIYIN <LIN SIYIN@LAPTOP-BNFDDN18>
2017-01-23 11:37:32 +0800

    view requisition list

---
+++ BusinessObject/ViewRequsitionListBO.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class ViewRequsitionListBO
+    {
+        private string requsitionId;
+        private string date;
+        private string name;
+        private string status;
+
+        public ViewRequsitionListBO(string requsitionId, string date, string name, string status)
+        {
+            this.requsitionId = requsitionId;
+            this.date = date;
+            this.name = name;
+            this.status = status;
+        }
+        public ViewRequsitionListBO() { }
+
+        public string RequsitionId
+        {
+            get
+            {
+                return requsitionId;
+            }
+
+            set
+            {
+                requsitionId = value;
+            }
+        }
+
+        public string Date
+        {
+            get
+            {
+                return date;
+            }
+
+            set
+            {
+                date = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Status
+        {
+            get
+            {
+                return status;
+            }
+
+            set
+            {
+                status = value;
+            }
+        }
+    }
+}
---
+++ DataAccess/ViewRequisitionListDA.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject;
+
+namespace DataAccess
+{
+    public class ViewRequisitionListDA
+    {
+        LogicUniversityEntities context = new LogicUniversityEntities();
+        public List<ViewRequsitionListBO> FindAllRequisition()
+        {
+            //q1 select the department head's department name
+            //the user id need to be changed into the current user id
+            var department = from x in context.Users where x.UserID == "D001" select x.Department;
+            string s = department.ToString();
+            
+
+            var q2 = (from r in context.Requisitions
+                      where
+                        r.User.DepartmentID == "COMM" &&
+                        r.Status == "Approved"
+                      select new
+                      {
+                          RID = r.RequisitionID,
+                          date = r.Date,
+                          name = r.User.Name,
+                          status = r.Status
+                      }).ToList();
+            List<ViewRequsitionListBO> rlist = new List<ViewRequsitionListBO>();
+            foreach(var data in q2)
+            {
+                ViewRequsitionListBO rbo = new ViewRequsitionListBO();
+                rbo.RequsitionId = data.RID;
+                rbo.Date = data.date.ToString();
+                rbo.Name = data.name;
+                rbo.Status = data.status;
+                rlist.Add(rbo);
+            }
+            return rlist;
+        }
+    }
+}
